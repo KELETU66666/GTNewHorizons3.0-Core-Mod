a65bcefa3e10ce999636aa03a8acc6c40ee95651
a <EYCHFSJD@gmail.com>
2023-03-08 18:46:29 +0800

    修复版本问题

--- src/main/java/com/github/newhorizons/common/item/itemList.java
+++ src/main/java/com/github/newhorizons/common/item/itemList.java
@@ -36,7 +36,8 @@ public class itemList extends Item {
             "itemWyvernSchematic",
             "itemAwakenedSchematic",
             "itemChaoticSchematic",
-            "itemRobotArm"
+            "itemRobotArm",
+            "itemAluminumRaw"
     };
 
     @Override
